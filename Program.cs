89acc065b035a504d0122065d60bdd2de8f69fca
Caspian2 <casa04001@utb.vaxjo.se>
2021-10-12 15:08:16 +0200

    v0.0.1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace guess_the_number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Do you wish to start the game? (Y/N)");
+            string yn = Console.ReadLine();
+
+            if(yn=="Y" || yn=="y"){
+                Console.WriteLine("works");
+            } else {
+                Console.Write("press any key to exit");
+                Console.ReadKey();
+            }
         }
     }
 }
