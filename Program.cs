d728d5bec13de3ff3ef3362d4bac9b1cc0e7fd89
Caspian2 <casa04001@utb.vaxjo.se>
2021-10-12 15:47:30 +0200

    finsished program

--- Program.cs
+++ Program.cs
@@ -10,7 +10,44 @@ namespace guess_the_number
             string yn = Console.ReadLine();
 
             if(yn=="Y" || yn=="y"){
-                Console.WriteLine("works");
+                //if started this is where game logic will be:
+                Random rnd = new Random();
+                int number = rnd.Next(1,100);
+                Console.WriteLine("Calculating the number you need to guess");
+                Console.Write("[#");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("#");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("#");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("#]");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine(" I have now chosen a number, what's your first guess?");
+
+                int guess = 0;
+                int tries = 0;
+
+                while(guess!=number) {
+                    guess = Int32.Parse(Console.ReadLine());
+
+                    if(guess>number){
+                        Console.WriteLine("too high!");
+                    } else if(guess<number){
+                        Console.WriteLine("too low!");
+                    } else{
+                        break;
+                    }
+                    tries++;
+
+                }
+                if(guess==number){
+                    Console.WriteLine($"Congrats you guessed the number {number} correctly, and it only took you {tries} to guess!");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("press any key to close program..");
+                    Console.ReadKey();
+                }
+
             } else {
                 Console.Write("press any key to exit");
                 Console.ReadKey();
