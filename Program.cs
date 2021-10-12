07f2723b88fe3bec40c9f75933c777aeb4e7a891
Caspian2 <casa04001@utb.vaxjo.se>
2021-10-12 15:00:18 +0200

    created and built project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace guess_the_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
