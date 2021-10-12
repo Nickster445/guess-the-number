b44b5035c538fa3354e2fe30c46fc0d29258c9b2
Caspian2 <casa04001@utb.vaxjo.se>
2021-10-12 15:56:13 +0200

    documented code

--- Program.cs
+++ Program.cs
@@ -11,8 +11,14 @@ namespace guess_the_number
 
             if(yn=="Y" || yn=="y"){
                 //if started this is where game logic will be:
+
+
+                //this is the logic behind choosing and defining a random number
                 Random rnd = new Random();
-                int number = rnd.Next(1,100);
+                int number = rnd.Next(1,101);
+
+                //logic behind the 'calculating' of a random number
+                //'System.Threading.Thread.Sleep(1000);' makes program wait 1000 miliseconds
                 Console.WriteLine("Calculating the number you need to guess");
                 Console.Write("[#");
                 System.Threading.Thread.Sleep(1000);
@@ -25,21 +31,29 @@ namespace guess_the_number
                 Console.WriteLine();
                 Console.WriteLine(" I have now chosen a number, what's your first guess?");
 
+                //defines integers for guess and tries, setting them to the defualt of 0
                 int guess = 0;
                 int tries = 0;
 
+                //while loop starts after the random number has been chosen
                 while(guess!=number) {
                     guess = Int32.Parse(Console.ReadLine());
 
+                    //if the number guessed is too high it writes the line of 'too high!' 
                     if(guess>number){
                         Console.WriteLine("too high!");
-                    } else if(guess<number){
+                    } 
+                    //if the number guessed is too high it writes the line of 'too low!'
+                    else if(guess<number){
                         Console.WriteLine("too low!");
-                    } else{
+                    } 
+                    //if the number is guessed it breaks out of the while loop
+                    else{
                         break;
                     }
                     tries++;
 
+                //handles logic for when number is guessed correctly
                 }
                 if(guess==number){
                     Console.WriteLine($"Congrats you guessed the number {number} correctly, and it only took you {tries} to guess!");
@@ -48,7 +62,9 @@ namespace guess_the_number
                     Console.ReadKey();
                 }
 
-            } else {
+            } 
+            //if you don't want the program to be started it exits at keystroke
+            else {
                 Console.Write("press any key to exit");
                 Console.ReadKey();
             }
